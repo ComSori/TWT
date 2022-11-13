433ba40096a88a337b8194b96d7ca2b4dbe0675b
jinwoo1234 <skdiwlsdn5@naver.com>
2022-11-13 23:10:24 +0900

    navbar

--- Resources/blaskan/functions.php
+++ Resources/blaskan/functions.php
@@ -162,7 +162,7 @@ function blaskan_scripts() {
 	wp_enqueue_script( 'masonry' );
 	wp_enqueue_script( 'blaskan-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 	wp_enqueue_script( 'blaskan-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
-	wp_enqueue_script( 'blaskan-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '20151215', true );
+	wp_enqueue_script( 'blaskan-scripts', get_template_directory_uri() . '/assets/js/navScripts.js', array( 'jquery' ), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
