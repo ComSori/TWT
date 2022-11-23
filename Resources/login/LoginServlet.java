c4dae6d4fc951b087ab9a77fba3f7d77ca28a126
jinwoo1234 <skdiwlsdn5@naver.com>
2022-11-23 14:45:45 +0900

    rename login page

--- Resources/login/LoginServlet.java
+++ Resources/login/LoginServlet.java
@@ -39,7 +39,7 @@ public class LoginServlet {
         out.print("</table></body></html>");
     }
 
-    /**
+    /*
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
