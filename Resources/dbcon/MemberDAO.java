e79ee09f071bfb3f3055a22dfec49b40bd437723
Jhsysng <ysng1118@naver.com>
2022-11-23 14:33:30 +0900

    login edit 0

--- Resources/dbcon/MemberDAO.java
+++ Resources/dbcon/MemberDAO.java
@@ -45,7 +45,7 @@ public class MemberDAO {
         try {
             Class.forName("com.mysql.driver");
             System.out.println("드라이버 로딩 성공");
-            con= DriverManager.getConnection("dbc:oracle:thin:@localhost:1521","alex","1592");
+            con= DriverManager.getConnection("dbc:oracle:thin:@localhost:1521","root","");
             System.out.println("Connection 생성 성공");
         }catch(Exception e) {
             e.printStackTrace();
