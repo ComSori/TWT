089181be712696b7859f0cd3bdfa0d808987b091
Jhsysng <ysng1118@naver.com>
2022-12-01 22:52:24 +0900

    team build session

--- Resources/tm/TeamBuildServlet.java
+++ Resources/tm/TeamBuildServlet.java
@@ -22,11 +22,15 @@ public class TeamBuildServlet extends HttpServlet {
         //session check
         HttpSession session = request.getSession(true);
         String s_uid=(String)session.getAttribute("id");
+        if(s_uid.equals("")){
+            PrintWriter out=response.getWriter();
+            out.println("<script>alert('login이 필요합니다.'); location.href='login_page.html';</script>");
+        }
 
         //null check
         if(r_tname.equals("")||r_tid.equals("")){
             PrintWriter out =response.getWriter();
-            out.println("<script>alert('id혹은이름을 공란없이 입력해주세요')</script>");
+            out.println("<script>alert('id혹은이름을 공란없이 입력해주세요');window.history.back();</script>");
         }
 
         //DB
@@ -35,8 +39,9 @@ public class TeamBuildServlet extends HttpServlet {
         if(dao.chk_team(r_tid).equals("")){
             dao.buildTeam(r_tname,r_tid,s_uid);
             session.setAttribute("team",r_tid);
+        }else{
+            PrintWriter out=response.getWriter();
+            out.println("<script>alert('이미 존재하는 id입니다.');window.history.back();</script>");
         }
-
-
     }
 }
--- Resources/tm/TeamJoinServlet.java
+++ Resources/tm/TeamJoinServlet.java
@@ -4,10 +4,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class TeamJoinServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=utf-8");
+
+        String r_tname=request.getParameter("team_name");
+        String r_tid=request.getParameter("team_id");
+        //session check
+        HttpSession session = request.getSession(true);
+        String s_uid=(String)session.getAttribute("t_id");
+        if(s_uid.equals("")){
+            PrintWriter out=response.getWriter();
+            out.println("<script>alert('login이 필요합니다.'); location.href='login_page.html';</script>");
+        }
     }
 }
